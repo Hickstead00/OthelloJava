52ed0234918003ad8bc100db76be5641e3d00dfa
Virgile <virgile.bodin@etu.univ-orleans.fr>
2025-02-27 00:00:49 +0100

    Ajout de l'affichage du plateau et test dans Main

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,4 +1,12 @@
 package main;
 
+import modele.Plateau;
+
 public class Main {
+    public static void main(String[] args) {
+        Plateau plateau = new Plateau(8);
+        plateau.afficherPlateau();
+
+    }
+
 }
