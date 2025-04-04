fa0ef0388b08a5edaaba4718a7960a4b76f64a84
guefb <guefb2@gmail.com>
2025-04-04 16:22:29 +0200

    manque juste le affame

--- src/modele/JeuAwale.java
+++ src/modele/JeuAwale.java
@@ -15,6 +15,16 @@ public class JeuAwale implements Jeu {
         initialiserJeu();
     }
 
+    public JeuAwale(JeuAwale copiePlateau ) {
+        this.plateau = new String[HAUTEUR][LARGEUR];
+        this.greniers = new int[2];
+        for (int i = 0; i < HAUTEUR; i++) {
+            for (int j = 0; j < LARGEUR; j++) {
+                this.plateau[i][j] = copiePlateau.plateau[i][j];
+            }
+        }
+    }
+
     @Override
     public void initialiserJeu() {
         // Initialiser le plateau avec 4 graines dans chaque trou
@@ -30,173 +40,77 @@ public class JeuAwale implements Jeu {
     }
 
     @Override
-    public boolean verifCoup(int ligne, int colonne, String couleur) {
-        // Vérifier si le coup est dans les limites
-        if (!estDansLesLimites(ligne, colonne)) {
+    public boolean verifCoup(int ligne,int colonne, String couleur)
+    {
+        if (!(estDansLesLimites(ligne,colonne)))
+        {
             return false;
         }
-        
-        // Vérifier si le joueur joue de son côté
-        if ((couleur.equals(COULEUR_J1) && ligne != 1) || 
-            (couleur.equals(COULEUR_J2) && ligne != 0)) {
+
+        if ((couleur.equals(COULEUR_J1) && ligne !=0) || (couleur.equals(COULEUR_J2) && ligne!=1))
+        {
             return false;
         }
-        
-        // Vérifier si le trou n'est pas vide
-        if (plateau[ligne][colonne].equals(CASE_VIDE) || 
-            Integer.parseInt(plateau[ligne][colonne]) == 0) {
+
+        if (plateau[ligne][colonne].equals(CASE_VIDE))
+        {
             return false;
         }
-        
-        // Vérifier si le coup n'affame pas l'adversaire
-        if (affameraitAdversaire(ligne, colonne)) {
-            return false;
-        }
-        
         return true;
     }
 
-    private boolean affameraitAdversaire(int ligne, int colonne) {
-        // Créer une copie du plateau pour simuler le coup
-        String[][] plateauCopie = new String[HAUTEUR][LARGEUR];
-        for (int i = 0; i < HAUTEUR; i++) {
-            for (int j = 0; j < LARGEUR; j++) {
-                plateauCopie[i][j] = plateau[i][j];
+    public void jouerCoup(int ligne,int colonne, String couleur)
+    {
+        int nbGraines= Integer.parseInt(plateau[ligne][colonne]);
+        plateau[ligne][colonne] = CASE_VIDE;
+        int ligneActual = ligne;
+        int colonneActual = colonne;
+
+        for (int i=0;i<=nbGraines;i++)
+        {
+            if (ligneActual == 0)
+            {
+                colonneActual = colonne--;
             }
-        }
-        
-        // Simuler le coup
-        int nbGraines = Integer.parseInt(plateauCopie[ligne][colonne]);
-        plateauCopie[ligne][colonne] = "0";
-        
-        int ligneActuelle = ligne;
-        int colonneActuelle = colonne;
-        
-        while (nbGraines > 0) {
-            // Passer au trou suivant (sens inverse des aiguilles d'une montre)
-            colonneActuelle--;
-            if (colonneActuelle < 0) {
-                ligneActuelle = (ligneActuelle == 0) ? 1 : 0;
-                colonneActuelle = 0;
-            } else if (colonneActuelle >= LARGEUR) {
-                ligneActuelle = (ligneActuelle == 0) ? 1 : 0;
-                colonneActuelle = LARGEUR - 1;
+            else
+            {
+                colonneActual = colonne++;
             }
-            
-            // Sauter le trou de départ si on fait un tour complet
-            if (ligneActuelle == ligne && colonneActuelle == colonne) {
+            /*si les graines sont 12 ou + saute la case de départ*/
+            if (nbGraines>11 && ligneActual == ligne && colonneActual == colonne)
+            {
                 continue;
             }
-            
-            // Déposer une graine
-            int valeur = Integer.parseInt(plateauCopie[ligneActuelle][colonneActuelle]);
-            plateauCopie[ligneActuelle][colonneActuelle] = String.valueOf(valeur + 1);
+            /*seme une graines sur la case*/
+            plateau[ligneActual][colonneActual] = String.valueOf(Integer.parseInt(plateau[ligneActual][colonneActual])+1);
             nbGraines--;
+
         }
-        
-        // Vérifier si l'adversaire a des graines
-        int ligneAdversaire = (ligne == 0) ? 1 : 0;
-        for (int j = 0; j < LARGEUR; j++) {
-            if (!plateauCopie[ligneAdversaire][j].equals("0") && 
-                Integer.parseInt(plateauCopie[ligneAdversaire][j]) > 0) {
-                return false; // L'adversaire a au moins une graine
-            }
+        int ligneJoueurAdverse = (couleur.equals(COULEUR_J1) ? 0 : 1;
+        if (ligneJoueurAdverse == ligneActual)
+        {
+            manger(ligneActual,colonneActual,couleur);
         }
-        
-        return true; // L'adversaire n'a plus de graines
+
     }
 
-    @Override
-    public void jouerCoup(int ligne, int colonne, String couleur) {
-        // Récupérer toutes les graines du trou
-        int nbGraines = Integer.parseInt(plateau[ligne][colonne]);
-        plateau[ligne][colonne] = "0";
-        
-        int ligneActuelle = ligne;
-        int colonneActuelle = colonne;
-        
-        // Distribuer les graines
-        while (nbGraines > 0) {
-            // Passer au trou suivant (sens inverse des aiguilles d'une montre)
-            colonneActuelle = (colonneActuelle + 1) % LARGEUR;
-            if (colonneActuelle == 0) {
-                ligneActuelle = (ligneActuelle == 0) ? 1 : 0;
-            }
-            
-            // Sauter le trou de départ si on fait un tour complet
-            if (ligneActuelle == ligne && colonneActuelle == colonne && nbGraines > 11) {
-                continue;
-            }
-            
-            // Déposer une graine
-            int valeur = Integer.parseInt(plateau[ligneActuelle][colonneActuelle]);
-            plateau[ligneActuelle][colonneActuelle] = String.valueOf(valeur + 1);
-            nbGraines--;
-        }
-        
-        // Vérifier s'il y a capture
-        int joueurIndex = couleur.equals(COULEUR_J1) ? 0 : 1;
-        int ligneAdversaire = (joueurIndex == 0) ? 0 : 1;
-        
-        // Capture et rafle
-        if (ligneActuelle == ligneAdversaire) {
-            rafle(ligneActuelle, colonneActuelle, joueurIndex);
-        }
-    }
-    
-    private void rafle(int ligne, int colonne, int joueurIndex) {
-        int valeur = Integer.parseInt(plateau[ligne][colonne]);
-        
-        // Si le trou contient 2 ou 3 graines, on les capture
-        if (valeur == 2 || valeur == 3) {
-            greniers[joueurIndex] += valeur;
-            plateau[ligne][colonne] = "0";
-            
-            // Vérifier si on peut continuer la rafle
-            if (colonne > 0) {
-                int valeurPrecedente = Integer.parseInt(plateau[ligne][colonne - 1]);
-                if (valeurPrecedente == 2 || valeurPrecedente == 3) {
-                    rafle(ligne, colonne - 1, joueurIndex);
-                }
+    private void manger(int ligneActual, int colonneActual, String couleur){
+        int idjoueur = couleur.equals(COULEUR_J1) ? 0 : 1 ;
+        int nbGrainesCase =Integer.parseInt(plateau[ligneActual][colonneActual]);
+        if (nbGrainesCase>1 && nbGrainesCase<4)
+        {
+
+            plateau[ligneActual][colonneActual].equals("0");
+            greniers[idjoueur] += nbGrainesCase;
+            if (colonneActual > 0)
+            {
+                    manger(ligneActual, colonneActual - 1, couleur);
             }
         }
     }
 
-    @Override
-    public boolean estPartieTerminee() {
-        // Vérifier si un joueur n'a plus de graines
-        boolean joueur1SansGraines = true;
-        boolean joueur2SansGraines = true;
-        
-        for (int j = 0; j < LARGEUR; j++) {
-            if (!plateau[1][j].equals("0") && Integer.parseInt(plateau[1][j]) > 0) {
-                joueur1SansGraines = false;
-            }
-            if (!plateau[0][j].equals("0") && Integer.parseInt(plateau[0][j]) > 0) {
-                joueur2SansGraines = false;
-            }
-        }
-        
-        // Si un joueur n'a plus de graines, l'autre capture les graines restantes
-        if (joueur1SansGraines || joueur2SansGraines) {
-            if (joueur1SansGraines) {
-                // Joueur 2 capture les graines restantes
-                for (int j = 0; j < LARGEUR; j++) {
-                    greniers[1] += Integer.parseInt(plateau[0][j]);
-                    plateau[0][j] = "0";
-                }
-            } else {
-                // Joueur 1 capture les graines restantes
-                for (int j = 0; j < LARGEUR; j++) {
-                    greniers[0] += Integer.parseInt(plateau[1][j]);
-                    plateau[1][j] = "0";
-                }
-            }
-            return true;
-        }
-        
-        return false;
-    }
+    private boolean siAffame()
+
 
     @Override
     public Joueur determinerVainqueur(Joueur joueurNoir, Joueur joueurBlanc) {
